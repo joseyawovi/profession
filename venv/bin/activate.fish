7f2c58c19d082b07a35e6e4303d192c4ee408f7e
joseyawovi <joseyawovi@gmail.com>
2024-07-17 13:28:53 +0530

    updated

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/user/Desktop/pro/profession/venv"
+set -gx VIRTUAL_ENV "/Users/user/Desktop/profession/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
